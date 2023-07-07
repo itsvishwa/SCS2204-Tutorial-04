ea7c572d9797394d4639ecf75dfd22a8b1b7fa9a
Vishwa Sandaruwan <116142492+itsvishwa@users.noreply.github.com>
2023-07-07 08:27:49 +0530

    Update Question01.scala
    fixed the percentages

--- tutorial04/src/main/scala/Question01.scala
+++ tutorial04/src/main/scala/Question01.scala
@@ -1,7 +1,7 @@
 def interest(amount: Double) = amount match {
     case x if x <= 20000 => x * 0.02;
-    case x if x <= 200000 => x * 0.04;
-    case x if x <= 2000000 => x * 0.035;
+    case x if x <= 200000 => x * 0.035;
+    case x if x <= 2000000 => x * 0.04;
     case _ => amount * 0.065;
 }
 
