96f822c24a77ec093d441ab59aa02125460c6a0d
Vishwa Sandaruwan <116142492+itsvishwa@users.noreply.github.com>
2023-07-08 23:27:31 +0530

    Update Question01.scala

--- tutorial04/src/main/scala/Question01.scala
+++ tutorial04/src/main/scala/Question01.scala
@@ -1,11 +1,12 @@
-def interest(amount: Double) = amount match {
-    case x if x <= 20000 => x * 0.02;
-    case x if x <= 200000 => x * 0.035;
-    case x if x <= 2000000 => x * 0.04;
-    case _ => amount * 0.065;
+def interest(amount: Double): Double = amount match {
+  case x if x <= 20000 => x * 0.02;
+  case x if x <= 200000 => x * 0.035;
+  case x if x <= 2000000 => x * 0.04;
+  case _ => amount * 0.065;
 }
 
-
-object Question01 extends App{
-    printf(f"Interset for Rs.4 000 000 = Rs. %%.2f", interest(4000000.0));
+object Question01 extends App {
+  println("Enter a number: ");
+  val data = scala.io.StdIn.readDouble();
+  printf(f"Interest = Rs. %.2f", interest(data));
 }
